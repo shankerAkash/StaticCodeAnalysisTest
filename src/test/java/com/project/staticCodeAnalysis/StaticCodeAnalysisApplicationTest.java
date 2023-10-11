cc52bbabe1267ec538d03faf9cfbada056195ef0
Akash Shanker <shankerakash1@gmail.com>
2023-10-11 23:03:35 +0100

    added test classs

--- src/test/java/com/project/staticCodeAnalysis/StaticCodeAnalysisApplicationTest.java
+++ src/test/java/com/project/staticCodeAnalysis/StaticCodeAnalysisApplicationTest.java
@@ -6,7 +6,6 @@ import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-@SpringBootConfiguration
 public class StaticCodeAnalysisApplicationTest {
 
 	@Test
