2d03e5694620ec9e66f564434de7703fa8162d78
Akash Shanker <shankerakash1@gmail.com>
2023-10-11 23:01:23 +0100

    updated classes

---
+++ src/main/java/com/project/staticCodeAnalysis/StaticCodeAnalysisApplication.java
@@ -0,0 +1,13 @@
+package com.project.staticCodeAnalysis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StaticCodeAnalysisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StaticCodeAnalysisApplication.class, args);
+	}
+
+}
