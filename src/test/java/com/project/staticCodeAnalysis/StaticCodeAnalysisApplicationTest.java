03253b5fb03464367bedb57c052254fe62a51c0c
Akash Shanker <shankerakash1@gmail.com>
2023-12-08 01:53:13 +0530

    Added Test Class
    Added Test Class

---
+++ src/test/java/com/project/staticCodeAnalysis/StaticCodeAnalysisApplicationTest.java
@@ -0,0 +1,15 @@
+package com.project.staticCodeAnalysis;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class StaticCodeAnalysisApplicationTest {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
